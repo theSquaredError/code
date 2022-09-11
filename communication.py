835c553ecac8773ca1534883594b593d5f45f7d3
theSquaredError <vikasaps.17@gmail.com>
2022-09-12 05:22:50 +0530

    function def

--- communication.py
+++ communication.py
@@ -1,10 +1,17 @@
-def vocab_generation():
-    pass
-
-
-
+import torch
+import constants
 # Communication module
-def mappings():
-    pass
 
 
+def generate_vocab(size):
+    return torch.rand(size)
+
+
+def mappings(n_locations):
+    '''
+    Takes the concepts vector and 
+    return a mapping for the corresponding vocabulary mapping and vice
+    '''
+    # vocab for each segment and quadrant is generated
+    octants_vocab = generate_vocab(constants.QUADRANT_SIZE)
+    segment_vocab = generate_vocab(constants.SEGMENT_SIZE)
