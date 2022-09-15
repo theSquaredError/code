d720aeb48fd5f7efb13b62a49ac16b0c568875ee
theSquaredError <vikasaps.17@gmail.com>
2022-09-15 18:32:43 +0530

    one hot encoding

--- agent_network.py
+++ agent_network.py
@@ -5,6 +5,10 @@ import numpy as np
 import torch.nn.functional as F
 from communication import Communication
 import constants
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from copy import deepcopy
 '''
 Both of the agent will learn their own mapping because each agent 
 is a separate neural network
@@ -13,10 +17,9 @@ is a separate neural network
 
 '''
 Neural network architecture:
-    Input Layer: [octant,segment]
-    Hidden Layer1: 5
-    Hidden Layer2: 5
-    Output Layer : size of vocabulary = num of octant * num of segment
+    Input Layer: 28 [one_hot encoding]
+    Hidden Layer1: 10
+    Output Layer : size of vocabulary = 28 [one_hot encoding]
 '''
 class MapNet(nn.Module):
     def __init__(self, input_size, output_size) -> None:
@@ -26,24 +29,65 @@ class MapNet(nn.Module):
         self.L2 = nn.Linear(10,output_size)
 
     def forward(self, input):
-        print(input)
+        # print(input)
         x = self.L1(input)
-        print(x)
+        # print(x)
         x = F.relu(x)
         x = self.L2(x)
-        print(x)
+        # print(x)
         x = F.softmax(x, dim=0)
         return x
 
+
+
+
+def print_loss(losses, learning_rate = 0.01):
+    import matplotlib.pyplot as plt
+    plt.plot(losses)
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.title("Learning rate %f"%(learning_rate))
+    plt.show()
+
+def train_agent(net, X,Y, learning_rate = 0.01):
+    loss_fn = nn.MSELoss()
+    optimiser = torch.optim.SGD(net.parameters(), lr=learning_rate)
+    losses = []
+    for epoch in range(10):
+        pred_y = net(X)
+        loss = loss_fn(pred_y, Y)
+        losses.append(loss.item())
+        
+        net.zero_grad()
+        loss.backward()
+        optimiser.step()
+    print_loss(losses)
+
+def one_hot_encoded(data):
+    dim = len(data)
+    temp = np.eye(dim)
+    return temp
+    
 
 if __name__ == '__main__':
     # Getting the mappings
     vocab_map = Communication.generate_vocabulary(constants.n_octants,constants.n_segments)
     # vocab_map = np.array(vocab_map)
-    X = [i[0] for i in vocab_map]
-    Y = [i[1] for i in vocab_map]
-    # print(vocab_map)
-    print(X)
-    # print(len(vocab_map))
-    # VocabNet = MapNet()
+    X_ = [i[0] for i in vocab_map]
+    Y_ = [i[1] for i in vocab_map]
+    
+    # Creating one hot encoding of each of the vector
+    X = torch.tensor(one_hot_encoded(X_), dtype=torch.float)
+    Y = torch.tensor(one_hot_encoded(Y_), dtype=torch.float)
+    
+
+    # VocabNet tries to learn the mapping from concept to vocab
+    # input size = output size = 8+20  = constants.n_octants+ constants.n_segments
+    vocabNet = MapNet(28,28)
+    # print(vocabNet)
+
+    train_agent(vocabNet,X,Y)
+
+
+    # train_agent(vocabNet,)
     # ConceptNet = MapNet()
